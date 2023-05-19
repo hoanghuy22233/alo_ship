a279548d8f726835d358530a2015a29bebf03b02
Duong <nguyendaiduong12345@gmail.com>
2023-05-19 15:48:34 +0700

    Duonwg

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -7,6 +7,7 @@ import 'package:sprint/screens/home/controllers/home_controller.dart';
 import 'package:sprint/screens/home/header_home.dart';
 import 'package:sprint/screens/home/item_list_home.dart';
 import 'package:sprint/services/entity/profile_response.dart';
+import 'package:sprint/widgets/DButton.dart';
 import 'package:sprint/widgets/app_base_page.dart';
 import 'package:sprint/widgets/app_text.dart';
 
@@ -49,6 +50,7 @@ class _HomePageState extends State<HomePage> {
                   onRefresh:this.onRefresh,
                   child: ListView.builder(
                     controller: _scrollController,
+                    physics: AlwaysScrollableScrollPhysics(),
                     itemCount: _homeController.dataBooking.value.length,
                     padding: EdgeInsets.only(top: 15.sp,left: 20.sp,right: 20.sp),
                     shrinkWrap: true,
@@ -61,7 +63,20 @@ class _HomePageState extends State<HomePage> {
 
               else{
                 return Center(
-                  child: AppText('Chưa có đơn hàng',style: AppStyle.DEFAULT_16.copyWith(fontStyle: FontStyle.italic),),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AppText('Chưa có đơn hàng',style: AppStyle.DEFAULT_16.copyWith(fontStyle: FontStyle.italic),),
+                      SizedBox(height: 20.sp,),
+                      Row(
+                        children: [
+                          Spacer(flex: 3,),
+                          Expanded(flex: 3,child: DButton(text: 'Làm mới', onClick: this.onRefresh)),
+                          Spacer(flex: 3,),
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }
             }),
